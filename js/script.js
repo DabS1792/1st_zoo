73c7b9b5629dced65fd867cdc2bc849200c69346
DabS1792 <dabsonwork2@gmail.com>
2021-09-14 16:25:37 +0300

    another commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $('.intro__slider').slick({
+        arrows: false,
+        dots: true,
+        speed: 500,
+        easing: 'ease',
+        Infinite: true,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        pauseOnFocus: true,
+        pauseOnHover: true,
+        pauseOnDotsHover: true,
+
+    });
+
+});
